e09b1f9c96f1e836c368e5ed2dc3f0a464c473fd
Nazar Buk <nazarbuk27@gmail.com>
2025-01-28 21:02:51 +0200

    Add port for site

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ export default defineConfig({
   plugins: [react()],
   base: "/forever-store/", //  // YOUR REPO NAME HERE
   //закоментуй base щоб дивитися проект локально
+  // server: { port: 5173 }, // щоб на цьому потрі запускався
 });
