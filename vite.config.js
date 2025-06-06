971f12f79df1ff0399e271c755dc1f8ff207d18e
Nazar Buk <nazarbuk27@gmail.com>
2025-06-06 20:44:35 +0300

    Change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/forever-store/", //  // YOUR REPO NAME HERE
+  base: "/",
+  // base: "/forever-store/", //  // YOUR REPO NAME HERE  //розкоментуй base щоб дивитися проект локально
   //закоментуй base щоб дивитися проект локально
   // server: { port: 5173 }, // щоб на цьому потрі запускався
 });
